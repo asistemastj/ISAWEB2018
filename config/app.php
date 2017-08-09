7d4fd15d83b7d59ca7bedc82f819347029e7f91f
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-08-09 14:24:50 -0500

    instalamos y configuramos LaravelCollective

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        #provider para poder usar LaravelCollective
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -226,6 +229,9 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        #aliases para LaravelCollective
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];

e962f4fbba8fb9e0c956edccdc101e4dd8714f6f
asistemas <asistemas@transportesjoselito.com>
2017-08-09 15:36:11 -0500

    creamos los controladores que usaremos, podriamos necesitar otros pero de momento creamos eso

---
+++ app/Http/Controllers/Archivo/ArchivoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Archivo;
+
+use App\Archivo;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ArchivoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Archivo  $archivo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Archivo $archivo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Archivo  $archivo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Archivo $archivo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Archivo  $archivo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Archivo $archivo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Archivo  $archivo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Archivo $archivo)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Caso/CasoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Caso;
+
+use App\Caso;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CasoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Caso  $caso
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Caso $caso)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Caso  $caso
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Caso $caso)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Caso  $caso
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Caso $caso)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Caso  $caso
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Caso $caso)
+    {
+        //
+    }
+}

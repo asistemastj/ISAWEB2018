54f1633fd98e63baaf0a5da284c6c88e9a806c06
asistemas <asistemas@transportesjoselito.com>
2017-08-10 18:13:26 -0500

    WARNING: actualizacion de envio, aun falta retoques pero en general ya funciona

--- app/Http/Controllers/User/UserEnviocontroller.php
+++ app/Http/Controllers/User/UserEnviocontroller.php
@@ -85,9 +85,32 @@ class UserEnvioController extends Controller
      * @param  \App\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, User $usuario)
+    public function update(Request $request, User $usuario, Envio $envio)
     {
-        //
+        #si el envio pertenece al usuario
+        if($usuario->id == $envio->user_id){
+            #validación
+            $this->validate($request, [
+                'contenido' => 'string',
+                'observacion' => 'string|min:6',
+                'destinatario_id'=>'required|integer'
+            ]);
+            #solo entran
+            $envio->fill($request->intersect([
+              'contenido',
+              'destinatario_id'
+            ]));
+            #si hay observacion
+            if($request->has('observacion')){
+              $envio->observacion = $request->observacion;
+            }
+            #ver si se modifico algo delenvio
+            if(!$envio->isClean()){
+              $envio->save();
+            }
+            #mostramos el nuevo envio
+            return response()->json(['data' => $envio, 'code' => 201]);
+        }
     }
 
     /**
@@ -100,7 +123,7 @@ class UserEnvioController extends Controller
     {
         if($usuario->id == $envio->user_id){
             $envio->delete();
-            return response()->json(['data' => $envio]);
+            return response()->json(['data' => $envio]);    
         }
     }
 }

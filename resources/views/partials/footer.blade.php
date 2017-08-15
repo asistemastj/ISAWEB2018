b1617091a0a281c580fe40405f15bb7553f8c651
asistemas <asistemas@transportesjoselito.com>
2017-08-15 09:58:58 -0500

    editamos footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,104 @@
+<!--Footer area starts Here -->
+			<footer id="footer">
+				<!--Footer box starts Here -->
+				<div class="footer  clearfix">
+					<div class="container">
+						<div class="row">
+							<div class="col-xs-12 col-sm-4">
+								<div class="quick-links">
+									<h5>NAVEGACIÓN</h5>
+								</div>
+								<div class="quick-list">
+									<ul>
+										<li>
+											<a href="{{route('home')}}">Inicio</a>
+										</li>
+										<li>
+											<a href="{{route('nosotros')}}">Nosotros</a>
+										</li>
+										<li>
+											<a href="{{route('servicios')}}">Servicios</a>
+										</li>
+										<li>
+											<a href="{{route('equipo')}}">Equipo</a>
+										</li>
+									</ul>
+								</div>
+								<div class="quick-list">
+									<ul>
+										<li>
+											<a href="{{route('ubicacion')}}">Ubicación</a>
+										</li>
+										<li>
+											<a href="{{route('logros')}}">Logros</a>
+										</li>
+										<li>
+											<a href="{{route('contacto')}}">Contáctenos</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-xs-12 col-sm-4">
+								<div class="our-address">
+									<h5>contactanos</h5>
+									<div class="address">
+										<h6>Transportes Joselito</h6>
+										<address>
+											Lote N°16-B2 Nro. 327 Ex-Fundo Larrea
+											La Libertad - Trujillo - Moche - Perú
+										</address>
+										<div class="phone">
+											<span>movil : <a href="994 029 631">994 029 631</a></span>
+											<span>email : <a href="mail">operaciones@transportesjoselito.com</a></span>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-xs-12 col-sm-4">
+								<div class="sign-up">
+									<h5>boletín de noticias</h5>
+									<p>
+										Si quieres recibir semananlmente nuestras nuevas 
+										ofertas, descuentos, escribenos.
+									</p>
+									<form>
+										<input id="mail" type="text" placeholder="Email Address" name="email" />
+										<!-- <input id="submit" type="submit" value="" class="fa fa-paper-plane-o" /> -->
+										<button class="fa fa-paper-plane"></button>
+									</form>
+								</div>
+							</div>
+						</div>
+						<div class="row custom-row">
+							<div class="col-xs-12 col-sm-5">
+								<div class="copyright">
+									<span>Copyright 2017. All Rights Reserved by <a href="#">Transportes Joselito.</a></span>
+								</div>
+							</div>
+							<div class="col-xs-12 col-sm-7">
+								<div class="footer-nav">
+									<ul>
+										<li>
+											<a href="#">terms of use</a>
+										</li>
+										<li>
+											<a href="#">legal desclaimer</a>
+										</li>
+										<li>
+											<a href="#">privacy policy</a>
+										</li>
+										<li>
+											<a href="#">support</a>
+										</li>
+										<li>
+											<a href="#">sitemap</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!--Footer box ends Here -->
+			</footer>
+			<!--Footer area ends Here -->

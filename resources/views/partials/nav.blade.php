4e88473e85093eb2c7c121f7886029a7a1ab0770
jefesistemas <aloayzaa26@gmail.com>
2017-08-19 13:31:31 -0500

    modificacion de logo

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,100 +1,100 @@
 <header id="header" class="header">
-				<!-- primary header Start Here -->
-				<div class="primary-header">
-					<div class="container">
-						<div class="row">
-							<div class="col-xs-12 col-sm-6">
-								<div class="mail">
-									<img src="images/icon-mail.png" alt="" />
-									<span>Escribenos al : <a class="email-us" href="#">operaciones@transportesjoselito.com</a></span>
-								</div>
-							</div>
-							<div class="col-xs-12 col-sm-6">
-								<div class="social-wrap clearfix">
-									<a href="request-a-quote.html" class="request">Solicita una cotización</a>
-									<ul class="social">
-										<li>
-											<a href="#"> <i class="fa fa-facebook"></i> </a>
-										</li>
-										<li>
-											<a href="#"> <i class="fa fa-twitter"></i> </a>
-										</li>
-										<li>
-											<a href="#"> <i class="fa fa-google-plus"></i> </a>
-										</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- primary header Ends Here -->
-				<!-- main header Starts Here -->
-				<div class="main-header">
-					<div class="container">
-						<div class="row">
-							<div class="col-xs-12 hidden-xs">
+    <!-- primary header Start Here -->
+    <div class="primary-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6">
+                    <div class="mail">
+                        <img src="images/icon-mail.png" alt="" />
+                        <span>Escribenos al : <a class="email-us" href="#">operaciones@transportesjoselito.com</a></span>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6">
+                    <div class="social-wrap clearfix">
+                        <a href="request-a-quote.html" class="request">Solicita una cotización</a>
+                        <ul class="social">
+                            <li>
+                                <a href="#"> <i class="fa fa-facebook"></i> </a>
+                            </li>
+                            <li>
+                                <a href="#"> <i class="fa fa-twitter"></i> </a>
+                            </li>
+                            <li>
+                                <a href="#"> <i class="fa fa-google-plus"></i> </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- primary header Ends Here -->
+    <!-- main header Starts Here -->
+    <div class="main-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 hidden-xs">
 
-								<div class="call-us">
-									<ul>
-										<li>
-											<img src="images/iphone.png" alt="" />
-											<span class="transport">LLAMANOS AHORA PARA <span></span> TU TRANSPORTE</span>
-										</li>
-										<li>
-											<a href="#">(044) 612240</a>											
-										</li>
-									</ul>
-								</div>
-							</div>
-							<div class="col-xs-12 col-sm-3">
-								<a href="{{route('home')}}" class="logo"> <img src="images/logo2.png" alt="" /> </a>
+                    <div class="call-us">
+                        <ul>
+                            <li>
+                                <img src="images/iphone.png" alt="" />
+                                <span class="transport">LLAMANOS AHORA PARA <span></span> TU TRANSPORTE</span>
+                            </li>
+                            <li>
+                                <a href="#">(044) 612240</a>											
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-3">
+                    <a href="{{route('home')}}" class="logo"> <img src="images/logo31.png" alt="" /> </a>
 
-							</div>
-							<div class="col-xs-12 col-sm-9 custom-nav">
-								<nav>
-									<div id='cssmenu'>
-										<ul class="navigation">
-											<li>
-												<a href="{{route('home')}}">Inicio</a>
-											</li>
-											<li>
-												<a href="{{route('nosotros')}}">Nosotros</a>
-											</li>
-											<li>
-												<a href="{{route('servicios')}}">Servicios</a>
-											</li>
-											<li>
-												<a href="{{route('ubicacion')}}">Ubicación</a>
-											</li>
-											<li>
-												<a href="#">Paginas</a>
-                                                <ul class="sub-menu">
-                                                    <li>
-														<a href="{{route('historia')}}">Historia</a>
-													</li>
-                                                    <li>
-														<a href="{{route('transporte')}}">Transporte</a>
-													</li>
-												</ul>
-											</li>
-											<li>
-												<a href="{{route('contacto')}}">Contactanos</a>
-											</li>
-											<li>
-												<a href="#">Intranet</a>
-											</li>
-										</ul>
-									</div>
-								</nav>
-								<div class="nav-icon">
-									<span></span>
-									<span></span>
-									<span></span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- main header Ends Here -->
-			</header>
+                </div>
+                <div class="col-xs-12 col-sm-9 custom-nav">
+                    <nav>
+                        <div id='cssmenu'>
+                            <ul class="navigation">
+                                <li>
+                                    <a href="{{route('home')}}">Inicio</a>
+                                </li>
+                                <li>
+                                    <a href="{{route('nosotros')}}">Nosotros</a>
+                                </li>
+                                <li>
+                                    <a href="{{route('servicios')}}">Servicios</a>
+                                </li>
+                                <li>
+                                    <a href="{{route('ubicacion')}}">Ubicación</a>
+                                </li>
+                                <li>
+                                    <a href="#">Paginas</a>
+                                    <ul class="sub-menu">
+                                        <li>
+                                            <a href="{{route('historia')}}">Historia</a>
+                                        </li>
+                                        <li>
+                                            <a href="{{route('transporte')}}">Transporte</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="{{route('contacto')}}">Contactanos</a>
+                                </li>
+                                <li>
+                                    <a href="#">Intranet</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <div class="nav-icon">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- main header Ends Here -->
+</header>
